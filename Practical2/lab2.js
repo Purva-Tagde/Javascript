632d0c0220724baf78b6c64d1b4f4c4b65b9e64a
Purva-Tagde <162147055+Purva-Tagde@users.noreply.github.com>
2025-11-11 17:32:45 +0530

    Add files via upload

---
+++ Practical2/lab2.js
@@ -0,0 +1,5 @@
+function concatenate() {
+  let a = document.getElementById("a").value;
+  let b = document.getElementById("b").value;
+  document.getElementById("result").innerHTML = "Concatenation: " + a + " " + b;
+}
